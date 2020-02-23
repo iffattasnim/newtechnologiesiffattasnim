5ee8b8908f939872cb84756896120417803dce5d
iffattasnim <43268596+iffattasnim@users.noreply.github.com>
2020-02-23 20:54:59 +0200

    Version 3

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
 var app = new Vue({
   el: '#app',
   data: {
+
+
     brand: 'Jute',
     product1: 'Rope',
     description1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     image1: './assets/jute.jpg',
-    inStock: true,
-    onSale: true,
+    inStock1: false,
+    onSale1: true,
     details: ['natural jute fiber', 'neutral color', 'hand washable'],
 
     product2: 'Sandal',
     description2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     image2: './assets/sandal.jpg',
-    inStock: true,
-    onSale: true,
+    inStock2: true,
+    onSale2: true,
 
     product3: 'yogamat',
     description3: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     image3: './assets/yogamat.jpg',
-    inStock: true,
-    onSale: true,
+    inStock3: false,
+    onSale3: true,
   }, 
   computed: {
     title(){
